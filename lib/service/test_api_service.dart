275afa80006653fe047ae1da97926f214fe98cbe
Paul_Luque <pluquec@unsa.edu.pe>
2024-12-03 14:29:15 -0500

    actualizando los test

--- lib/service/test_api_service.dart
+++ lib/service/test_api_service.dart
@@ -48,8 +48,7 @@ class TestApiService extends StatelessWidget {
                 onPressed: () async {
                   try {
                     final waterVolume = await apiService.getWaterVolume();
-                    print('Water volume: $waterVolume');
-                    print(waterVolume.volume);
+                    print('Water volume: ${waterVolume.volume}');
                   } catch (e) {
                     print('Error fetching water volume: $e');
                   }
@@ -60,14 +59,59 @@ class TestApiService extends StatelessWidget {
                 onPressed: () {
                   try {
                     apiService.getWaterVolumeStream().listen((waterVolume) {
-                      print('Real-time water volume: $waterVolume');
-                      print(waterVolume.volume);
+                      print('Real-time water volume: ${waterVolume.volume}');
                     });
                   } catch (e) {
                     print('Error fetching real-time water volume: $e');
                   }
                 },
                 child: Text('Fetch Water Volume Real-Time'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final node = await apiService.getNode(1);
+                    print('Node 1 data: ${node.toJson()}');
+                  } catch (e) {
+                    print('Error fetching node 1 data: $e');
+                  }
+                },
+                child: Text('Fetch Node 1 Once'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  try {
+                    apiService.getNodeRealTime(1).listen((node) {
+                      print('Real-time Node 1 data: ${node.toJson()}');
+                    });
+                  } catch (e) {
+                    print('Error fetching real-time node 1 data: $e');
+                  }
+                },
+                child: Text('Fetch Node 1 Real-Time'),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final node = await apiService.getNode(2);
+                    print('Node 2 data: ${node.toJson()}');
+                  } catch (e) {
+                    print('Error fetching node 2 data: $e');
+                  }
+                },
+                child: Text('Fetch Node 2 Once'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  try {
+                    apiService.getNodeRealTime(2).listen((node) {
+                      print('Real-time Node 2 data: ${node.toJson()}');
+                    });
+                  } catch (e) {
+                    print('Error fetching real-time node 2 data: $e');
+                  }
+                },
+                child: Text('Fetch Node 2 Real-Time'),
               ),
             ],
           ),
